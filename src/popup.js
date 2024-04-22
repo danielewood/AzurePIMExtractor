f4419c3d27a4373d5e6f58d1e29f7d13c61a2441
Daniel Wood <daniel@woods.us>
2024-04-22 14:32:08 -0500

    Update extension behavior

--- src/popup.js
+++ src/popup.js
@@ -1,22 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
-  chrome.storage.local.get('tokenObj', function (res) {
-    var tokenElem = document.getElementById('token');
-    var dateElem = document.getElementById('date_acquired');
-    console.log(JSON.stringify(res));
-    tokenElem.value = res.tokenObj.token;
-    dateElem.innerHTML = 'Acquired: ' + res.tokenObj.date;
+  const targetUrl = 'https://portal.azure.com/#view/Microsoft_Azure_PIMCommon/ActivationMenuBlade/~/aadgroup/provider/aadgroup';
 
-    // Select the token text
-    tokenElem.focus();
-    tokenElem.select();
-    document.getElementById('token').focus();
-    document.getElementById('copyButton').addEventListener('click', function () {
-      navigator.clipboard.writeText(tokenElem.value).then(function () {
-        console.log('Text successfully copied to clipboard');
-      }).catch(function (err) {
-        console.error('Unable to copy text to clipboard', err);
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.storage.local.get('tokenObj', function (res) {
+      var tokenElem = document.getElementById('token');
+      var expiresElem = document.getElementById('minutes_remaining');
+      var ticketElem = document.getElementById('ticket');
+
+      tokenElem.value = res.tokenObj.token;
+      const decodedJwt = parseJwt(tokenElem.value);
+      const now = Math.floor(Date.now() / 1000);
+      const minutesRemaining = Math.floor((decodedJwt.exp - now) / 60);
+
+      expiresElem.innerText = minutesRemaining > 0 ? `${minutesRemaining} minutes` : "expired";
+
+      if (decodedJwt.exp > now) {
+        // Token is valid, automatically copy to clipboard
+        navigator.clipboard.writeText(tokenElem.value).then(function () {
+          console.log('Token automatically copied to clipboard');
+        }).catch(function (err) {
+          console.error('Failed to automatically copy token to clipboard', err);
+        });
+      } else if (tabs[0].url !== targetUrl) {
+        // Redirect only if the token is expired and we're not on the target URL
+        chrome.tabs.create({url: targetUrl});
+      }
+
+      document.getElementById('copyButton').addEventListener('click', function () {
+        navigator.clipboard.writeText(tokenElem.value);
+      });
+
+      document.getElementById('saveButton').addEventListener('click', function () {
+        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify({
+          jwt: tokenElem.value,
+          ticket: ticketElem.value
+        }));
+        var downloader = document.createElement('a');
+        downloader.href = dataStr;
+        downloader.download = 'azurejwt.json';
+        downloader.click();
       });
     });
   });
-});
 
+  function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  }
+});
